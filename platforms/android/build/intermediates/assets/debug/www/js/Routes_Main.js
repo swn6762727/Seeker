1732342f93242c57a17565521ab1838eb8c0e9b9
Lancelot Song <swn6762727@gmail.com>
2016-06-05 20:57:24 +0800

    Version on June 5

--- platforms/android/build/intermediates/assets/debug/www/js/Routes_Main.js
+++ platforms/android/build/intermediates/assets/debug/www/js/Routes_Main.js
@@ -1,65 +1,56 @@
 angular.module('app.routes', [])
 
-.config(function($stateProvider, $urlRouterProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-
-
-      .state('tabsController.page2', {
-    url: '/discover',
-    views: {
-      'tab4': {
-        templateUrl: 'Main/Discover.html',
-        controller: 'page2Ctrl'
-      }
-    }
-  })
-
-  .state('tabsController.page3', {
-    url: '/add',
-    views: {
-      'tab5': {
-        templateUrl: 'Main/Submit.html',
-        controller: 'page3Ctrl'
-      }
-    }
-  })
-
-  .state('tabsController.page4', {
-    url: '/interest_area',
-    views: {
-      'tab6': {
-        templateUrl: 'Main/Community.html',
-        controller: 'page4Ctrl'
-      }
-    }
-  })
-
-  .state('tabsController', {
-    url: '/page1',
-    templateUrl: 'Main/tabsController.html',
-    abstract:true
-  })
-
-  .state('page7', {
-    url: '/logon',
-    templateUrl: 'Main/Logon.html',
-    controller: 'page7Ctrl'
-  })
-
-  .state('page9', {
-    url: '/interest_list',
-    templateUrl: 'Main/interest_list.html',
-    controller: 'page9Ctrl'
-  })
-
-$urlRouterProvider.otherwise('/page1/add')
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
 
 
+            .state('tabsController.page2', {
+                url: '/discover',
+                views: {
+                    'tab4': {
+                        templateUrl: 'Main/Discover.html',
+                        controller: 'page2Ctrl'
+                    }
+                }
+            })
 
-});
+            .state('tabsController.page3', {
+                url: '/add',
+                views: {
+                    'tab5': {
+                        templateUrl: 'Main/Submit.html',
+                        controller: 'page3Ctrl'
+                    }
+                }
+            })
+
+            .state('tabsController.page4', {
+                url: '/community',
+                views: {
+                    'tab6': {
+                        templateUrl: 'Main/Community.html',
+                        controller: 'page4Ctrl'
+                    }
+                }
+            })
+
+            .state('tabsController', {
+                url: '/page1',
+                templateUrl: 'Main/tabsController.html',
+                abstract: true
+            })
+
+
+            .state('interest_list', {
+                url: '/interest_list',
+                templateUrl: 'Main/Interest_list.html'
+            });
+            $urlRouterProvider.otherwise('/page1/community')
+
+
+    });
--- platforms/ios/build/emulator/趣客.app/www/js/controllers.js
+++ platforms/ios/build/emulator/趣客.app/www/js/controllers.js
@@ -33,3 +33,4 @@ angular.module('app.controllers', [])
 
     })
 
+
